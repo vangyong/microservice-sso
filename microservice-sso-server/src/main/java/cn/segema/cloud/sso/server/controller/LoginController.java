21d87a3e09ed105cc612959dfed086e9d01c40bf
vangyong <vangyong@126.com>
2020-03-21 22:10:13 +0800

    修改测试

--- microservice-sso-server/src/main/java/cn/segema/cloud/sso/server/controller/LoginController.java
+++ microservice-sso-server/src/main/java/cn/segema/cloud/sso/server/controller/LoginController.java
@@ -1,19 +1,23 @@
 package cn.segema.cloud.sso.server.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
+	private static Logger logger = LoggerFactory.getLogger(LoginController.class);
 
-    @GetMapping("/index")
-    public String index() {
-        return "index";
-    }
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
 
 }
